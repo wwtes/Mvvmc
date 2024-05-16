8f543d34cfeca30e669e1287a4269c57651a2cbb
wwtes <cristyanraffael1000@gmail.com>
2024-05-16 08:33:41 -0300

    Update playnormal.js

--- commands/playnormal.js
+++ commands/playnormal.js
@@ -2,17 +2,17 @@ const { ApplicationCommandOptionType, EmbedBuilder } = require('discord.js');
 const db = require("../mongoDB");
 module.exports = {
   name: "playsong",
-  description: "Play a track.",
+  description: "Toque uma faixa.",
   permissions: "0x0000000000000800",
   options: [
     {
       name: "normal",
-      description: "Open music from other platforms.",
+      description: "Abra músicas de outras plataformas.",
       type: ApplicationCommandOptionType.Subcommand,
       options: [
         {
           name: "name",
-          description: "Write your music name.",
+          description: "Escreva o nome da sua música.",
           type: ApplicationCommandOptionType.String,
           required: true
         }
@@ -20,12 +20,12 @@ module.exports = {
     },
     {
       name: "playlist",
-      description: "Write your playlist name.",
+      description: "Escreva o nome da sua playlist.",
       type: ApplicationCommandOptionType.Subcommand,
       options: [
         {
           name: "name",
-          description: "Write the name of the playlist you want to create.",
+          description: "Escreva o nome da lista de reprodução que deseja criar.",
           type: ApplicationCommandOptionType.String,
           required: true
         }
@@ -44,7 +44,7 @@ module.exports = {
       if (stp === "playlist") {
         let playlistw = interaction.options.getString('name')
         let playlist = await db?.playlist?.find().catch(e => { })
-        if (!playlist?.length > 0) return interaction.reply({ content: `There is no playlist. ❌`, ephemeral: true }).catch(e => { })
+        if (!playlist?.length > 0) return interaction.reply({ content: `Não há lista de reprodução. ❌`, ephemeral: true }).catch(e => { })
 
         let arr = 0
         for (let i = 0; i < playlist.length; i++) {
@@ -62,9 +62,9 @@ module.exports = {
             const music_filter = playlist[i]?.musics?.filter(m => m.playlist_name === playlistw)
             if (!music_filter?.length > 0) return interaction.reply({ content: `No music with Name`, ephemeral: true }).catch(e => { })
                 const listembed = new EmbedBuilder()
-                .setTitle('Loading Your Album')
+                .setTitle('Carregando seu álbum')
                 .setColor('#FF0000')
-                .setDescription('**🎸 Get ready for a musical journey!**');
+                .setDescription('**🎸 Prepare-se para uma jornada musical!**');
             interaction.reply({ content : '', embeds: [listembed] }).catch(e => { })
 
             let songs = []
@@ -78,12 +78,12 @@ module.exports = {
               });
               const qembed = new EmbedBuilder()
         .setAuthor({
-        name: 'Added Album Songs to Queue',
+        name: 'Adicionadas músicas do álbum à fila',
         iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157218651179597884/1213-verified.gif', 
-        url: 'https://discord.gg/FUEHs7RCqz'
+        url: ''
     })
         .setColor('#14bdff')
-        .setFooter({ text: 'Use /queue for more Information' });
+        .setFooter({ text: 'Use /queue para mais informações' });
            
               await interaction.editReply({ content: '',embeds: [qembed] }).catch(e => {
                   console.error('Error  reply:', e);
@@ -96,7 +96,7 @@ module.exports = {
                   interaction
                 })
               } catch (e) {
-                await interaction.editReply({ content: `❌ No results found!!`, ephemeral: true }).catch(e => { })
+                await interaction.editReply({ content: `❌ Nenhum resultado encontrado!!`, ephemeral: true }).catch(e => { })
               }
 
               playlist[i]?.playlist?.filter(p => p.name === playlistw).map(async p => {
@@ -125,7 +125,7 @@ module.exports = {
           } else {
             arr++
             if (arr === playlist.length) {
-              return interaction.reply({ content: `There is no Album ❌`, ephemeral: true }).catch(e => { })
+              return interaction.reply({ content: `Não há álbum ❌`, ephemeral: true }).catch(e => { })
             }
           }
         }
@@ -134,12 +134,12 @@ module.exports = {
       if (stp === "normal") {
   const name = interaction.options.getString('name');
   if (!name) {
-    return interaction.reply({ content: '▶️ Give Text or link', ephemeral: true }).catch(e => {});
+    return interaction.reply({ content: '▶️ Dê texto ou link', ephemeral: true }).catch(e => {});
   }
 
   const embed = new EmbedBuilder()
     .setColor('#FF0000')
-    .setDescription('**🎸 Get ready for a musical journey!**');
+    .setDescription('**🎸 Prepare-se para uma jornada musical!**');
 
   await interaction.reply({ embeds: [embed] }).catch(e => {});
 
@@ -152,7 +152,7 @@ module.exports = {
   } catch (e) {
     const errorEmbed = new EmbedBuilder()
       .setColor('#FF0000')
-      .setDescription('❌ No results found!!');
+      .setDescription('❌ Nenhum resultado encontrado!!');
 
     await interaction.editReply({ embeds: [errorEmbed], ephemeral: true }).catch(e => {});
   }
