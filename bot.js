d016092545c863edaf1637001adf463ccc97d78c
wwtes <cristyanraffael1000@gmail.com>
2024-05-16 08:44:05 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -129,7 +129,7 @@ app.get('/', (req, res) => {
   res.sendFile(imagePath);
 });
 app.listen(port, () => {
-  console.log(`🔗 Listening to GlaceYT: http://localhost:${port}`);
+  console.log(`🔗 Ouvindo Mvvmc: http://localhost:${port}`);
 });
 printWatermark();
 
