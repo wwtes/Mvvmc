183a802c891bc9915daae2b88bd80945b930ff0c
wwtes <cristyanraffael1000@gmail.com>
2024-05-16 08:36:15 -0300

    Update resume.js

--- commands/resume.js
+++ commands/resume.js
@@ -3,7 +3,7 @@ const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
   name: "resume",
-  description: "Start paused music.",
+  description: "Inicie a música pausada.",
   permissions: "0x0000000000000800",
   options: [],
   voiceChannel: true,
@@ -12,11 +12,11 @@ module.exports = {
 
     try {
       if (!queue) {
-        return interaction.reply({ content: '⚠️ Queue is empty!!', ephemeral: true });
+        return interaction.reply({ content: '⚠️ A fila está vazia!!', ephemeral: true });
       }
 
       if (!queue.paused) {
-        return interaction.reply({ content: '⚠️ No paused music!!', ephemeral: true });
+        return interaction.reply({ content: '⚠️ Sem música pausada!!', ephemeral: true });
       }
 
       const success = queue.resume();
@@ -26,9 +26,9 @@ module.exports = {
         .setAuthor({
           name: 'Song Resumed',
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157296313549983846/8929-purple-play-icon.png?ex=651817ae&is=6516c62e&hm=55fc041718da9277d1cdb13ef25ebf043d90588ee33c4bc838d9634ecfbc8e99&',
-          url: 'https://discord.gg/FUEHs7RCqz'
+          url: ''
         })
-        .setDescription(success ? '**The music springs back to life!!**' : '❌ Error: Unable to resume song')
+        .setDescription(success ? '**A música volta à vida!!**' : '❌ Erro: Não é possível retomar a música')
         
 
       return interaction.reply({ embeds: [embed] });
