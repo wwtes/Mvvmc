7678629c2e5412e18e97b7a8b784e0caf3c7d747
wwtes <cristyanraffael1000@gmail.com>
2024-05-16 08:57:04 -0300

    Update filter.js

--- commands/filter.js
+++ commands/filter.js
@@ -54,7 +54,7 @@ module.exports = {
       .setColor('#01fe66')
       .setAuthor({
           name: 'Audio Filters ',
-          iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157534645311766558/2353-arrowrightglow.gif?ex=6518f5a5&is=6517a425&hm=ce55696f7ed85e2f7a97a3505eb39016fa9cd0c50be043efdf0cce06d7126b4c&',
+          iconURL: '',
           url: ''
         })
       .setDescription('** Explore a batida, escolha sua magia sonora abaixo!**')
