b7c3be89081dbe40fd8b1122135e182c70fea729
wwtes <cristyanraffael1000@gmail.com>
2024-05-16 08:42:59 -0300

    Update volume.js

--- commands/volume.js
+++ commands/volume.js
@@ -26,11 +26,11 @@ const db = require("../mongoDB");
 
 module.exports = {
   name: "volume",
-  description: "Allows you to adjust the music volume.",
+  description: "Permite ajustar o volume da música.",
   permissions: "0x0000000000000800",
   options: [{
     name: 'volume',
-    description: 'Type the number to adjust the volume.',
+    description: 'Digite o número para ajustar o volume.',
     type: ApplicationCommandOptionType.Integer,
     required: true
   }],
@@ -39,25 +39,25 @@ module.exports = {
     try {
       const queue = client.player.getQueue(interaction.guild.id);
       if (!queue || !queue.playing) {
-        return interaction.reply({ content: '⚠️ No music playing!!', ephemeral: true });
+        return interaction.reply({ content: '⚠️ Nenhuma música tocando!!', ephemeral: true });
       }
 
       const vol = parseInt(interaction.options.getInteger('volume'));
 
       if (!vol) {
         return interaction.reply({
-          content: `Current volume: **${queue.volume}** 🔊\nTo change the volume, type a number between \`1\` and \`${maxVol}\`.`,
+          content: `Current volume: **${queue.volume}** 🔊\nPara alterar o volume, digite um número entre \`1\` e \`${maxVol}\`.`,
           ephemeral: true
         });
       }
 
       if (queue.volume === vol) {
-        return interaction.reply({ content: 'Current volume is already set to **' + vol + '**!', ephemeral: true });
+        return interaction.reply({ content: 'O volume atual já está definido para **' + vol + '**!', ephemeral: true });
       }
 
       if (vol < 1 || vol > maxVol) {
         return interaction.reply({
-          content: `Please type a number between \`1\` and \`${maxVol}\`.`,
+          content: `Por favor digite um número entre \`1\` e \`${maxVol}\`.`,
           ephemeral: true
         });
       }
@@ -68,15 +68,15 @@ module.exports = {
         const embed = new EmbedBuilder()
           .setColor('#d291fe')
           .setAuthor({
-        name: 'Your Music! Your Rules!',
+        name: 'Sua música! Suas regras!',
         iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157528025739563088/5657-volume-icon.png?ex=6518ef7b&is=65179dfb&hm=1797c2830537a28b5c6a57564517cc509146d02383a69fb4239d7b5d55aceeed&', 
-        url: 'https://discord.gg/FUEHs7RCqz'
+        url: ''
     })
-          .setDescription(`**Adjusting Volume : ** **${vol}/${maxVol}**`);
+          .setDescription(`**Ajustando o volume : ** **${vol}/${maxVol}**`);
 
         return interaction.reply({ embeds: [embed] });
       } else {
-        return interaction.reply({ content: '❌ Something went wrong while changing the volume.', ephemeral: true });
+        return interaction.reply({ content: '❌ Algo deu errado ao alterar o volume.', ephemeral: true });
       }
     } catch (e) {
       console.error(e);
