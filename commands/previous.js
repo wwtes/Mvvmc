c3790e89e71b3dc839f2d932c82add9df86bdc21
wwtes <cristyanraffael1000@gmail.com>
2024-05-16 08:34:19 -0300

    Update previous.js

--- commands/previous.js
+++ commands/previous.js
@@ -1,19 +1,19 @@
 const db = require("../mongoDB");
 module.exports = {
   name: "previous",
-  description: "Plays the previous track.",
+  description: "Reproduz a faixa anterior.",
   permissions: "0x0000000000000800",
   options: [],
   voiceChannel: true,
   run: async (client, interaction) => {
     try {
       const queue = client.player.getQueue(interaction.guild.id);
-      if (!queue || !queue.playing) return interaction.reply({ content: `⚠️ No music playing!!`, ephemeral: true }).catch(e => { })
+      if (!queue || !queue.playing) return interaction.reply({ content: `⚠️ Nenhuma música tocando!!`, ephemeral: true }).catch(e => { })
       try {
         let song = await queue.previous()
-        interaction.reply({ content: `**Behold, the enchanting melody of the past!!**` }).catch(e => { })
+        interaction.reply({ content: `**Eis a melodia encantadora do passado!!**` }).catch(e => { })
       } catch (e) {
-        return interaction.reply({ content: `❌ No previous track!!`, ephemeral: true }).catch(e => { })
+        return interaction.reply({ content: `❌ Nenhuma faixa anterior!!`, ephemeral: true }).catch(e => { })
       }
     } catch (e) {
     console.error(e); 
