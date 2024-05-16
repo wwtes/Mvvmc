610b0ee619cc6d827672fe12389f2de8404a224c
wwtes <cristyanraffael1000@gmail.com>
2024-05-16 08:08:04 -0300

    Update error.js

--- events/player/error.js
+++ events/player/error.js
@@ -1,6 +1,6 @@
 module.exports = async (client, textChannel, e) => {
 if (textChannel){
-   return textChannel?.send(`**An error encountered:** ${e.toString().slice(0, 1974)}`)
+   return textChannel?.send(`**Um erro encontrad:** ${e.toString().slice(0, 1974)}`)
 }
 }
 
