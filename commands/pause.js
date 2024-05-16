59c95e44962454497edbfa158ffaaf4777dc4aa5
wwtes <cristyanraffael1000@gmail.com>
2024-05-16 08:21:48 -0300

    Update pause.js

--- commands/pause.js
+++ commands/pause.js
@@ -23,7 +23,7 @@ const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
   name: "pause",
-  description: "Stops playing the currently playing music.",
+  description: "Pára de reproduzir a música que está sendo reproduzida.",
   permissions: "0x0000000000000800",
   options: [],
   voiceChannel: true,
@@ -32,7 +32,7 @@ module.exports = {
 
     try {
       if (!queue || !queue.playing) {
-        return interaction.reply({ content: '⚠️ No music playing!!', ephemeral: true });
+        return interaction.reply({ content: '⚠️ Nenhuma música tocando!!', ephemeral: true });
       }
 
       const success = queue.pause();
@@ -40,11 +40,11 @@ module.exports = {
       const embed = new EmbedBuilder()
         .setColor('#7645fe') 
         .setAuthor({
-          name: 'Song Paused',
+          name: 'Música pausada',
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157296313013117049/8061-purple-pause-icon.png?ex=651817ae&is=6516c62e&hm=4596c9fab9d8b66de8b5215b2750572ced352eed67440a1134550b846b5693b9&',
-          url: 'https://discord.gg/FUEHs7RCqz'
+          url: ''
         })
-        .setDescription(success ? '**The music has been Paused for a moment!!**' : '❌ Command Error: Unable to pause song')
+        .setDescription(success ? '**A música foi pausada por um momento!!**' : '❌ Command Error: Não foi possível pausar a música')
         
 
       return interaction.reply({ embeds: [embed] });
