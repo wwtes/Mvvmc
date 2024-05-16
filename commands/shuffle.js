9eecfdfee79f2caa0d9d1180f4d03db5e975c681
wwtes <cristyanraffael1000@gmail.com>
2024-05-16 08:37:37 -0300

    Update shuffle.js

--- commands/shuffle.js
+++ commands/shuffle.js
@@ -3,17 +3,17 @@ const config = require("../config.js");
 const db = require("../mongoDB");
 module.exports = {
   name: "shuffle",
-  description: "Shuffle the queue songs",
+  description: "Embaralhe as músicas da fila",
   options: [],
   permissions: "0x0000000000000800",
   run: async (client, interaction) => {
     try {
 
         const queue = client.player.getQueue(interaction.guild.id);
-        if (!queue || !queue.playing) return interaction.reply({ content: `⚠️ No music playing!!`, ephemeral: true }).catch(e => { })
+        if (!queue || !queue.playing) return interaction.reply({ content: `⚠️ Nenhuma música tocando!!`, ephemeral: true }).catch(e => { })
         try {
           queue.shuffle(interaction)
-        return interaction.reply({ content: `<@${interaction.user.id}>, Shuffled the songs for You!!` }).catch(e => { })
+        return interaction.reply({ content: `<@${interaction.user.id}>, Embaralhei as músicas para você!!` }).catch(e => { })
         } catch(err) {
         return interaction.reply({ content: `**${err}**` }).catch(e => { })
         }
