c5fd85abf3a1d0eb03fb46312383df5962bcdce3
wwtes <cristyanraffael1000@gmail.com>
2024-05-16 08:58:10 -0300

    Update loop.js

--- commands/loop.js
+++ commands/loop.js
@@ -50,8 +50,8 @@ module.exports = {
       const embed = new EmbedBuilder()
         .setColor('#fc4e03')
         .setAuthor({
-        name: 'Faça um loop em suas melodias',
-        iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157318080670728283/7905-repeat.gif?ex=65182bf4&is=6516da74&hm=9ae58f40fcea5dc42a2a992bbd159d50116b3bafe5c5f7728e3a5276442efd2a&', 
+        name: 'Loop your melodies',
+        iconURL: '', 
         url: ''
     })
         .setDescription('**Fazendo um loop! Deixe a música tocar sem parar. **')
